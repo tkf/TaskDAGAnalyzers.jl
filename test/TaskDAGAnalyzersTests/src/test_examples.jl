a5273f4558cfaed6b943343111f8573ff708a55c
Takafumi Arakaki <aka.tkf@gmail.com>
2021-10-16 23:46:30 -0400

    Test work and span

--- test/TaskDAGAnalyzersTests/src/test_examples.jl
+++ test/TaskDAGAnalyzersTests/src/test_examples.jl
@@ -4,11 +4,12 @@ import ShowGraphviz
 import TaskDAGAnalyzers
 import TaskDAGRecorders
 using TaskDAGAnalyzers: Examples
+using TaskDAGAnalyzers.Internal: SpawnNode, SequentialNode, DurationNS, foreachnode
 using Test
 
 function check_smoke(f)
     ctx = TaskDAGRecorders.sample(f)
-    dag = TaskDAGAnalyzers.dag(ctx)    
+    dag = TaskDAGAnalyzers.dag(ctx)
     dot = ShowGraphviz.DOT(dag)
     src = sprint(show, "text/plain", dot)
     @test occursin("digraph", src)
@@ -22,6 +23,49 @@ function test_smoke()
     @testset for f in [Examples.dac, Examples.dac4]
         check_smoke(f)
     end
+end
+
+function set_uniform_unit_work!(dag, unit)
+    foreachnode(dag) do node
+        if node isa SequentialNode
+            node.data = unit
+        elseif node isa SpawnNode
+            node.data = DurationNS(typemax(Int))  # this should be ignored
+        end
+    end
+end
+
+# TODO: don't depend on noisefloor
+function check_unit_span(f)
+    ctx = TaskDAGRecorders.sample(f, 4)[end]
+    dag = TaskDAGAnalyzers.dag(ctx)
+    unit = DurationNS(123_456_789)
+    set_uniform_unit_work!(dag, unit)
+    @test TaskDAGAnalyzers.span(dag) == unit
+end
+
+function test_unit_span()
+    @testset for f in [Examples.dac, Examples.dac4]
+        check_unit_span(f)
+    end
+end
+
+function test_dac_work()
+    ctx = TaskDAGRecorders.sample(Examples.dac, 4)[end]
+    dag = TaskDAGAnalyzers.dag(ctx)
+    unit = DurationNS(123_456_789)
+    set_uniform_unit_work!(dag, unit)
+    expected_work = DurationNS(unit.value * 4)
+    @test TaskDAGAnalyzers.work(dag) == expected_work
+end
+
+function test_dac4_work()
+    ctx = TaskDAGRecorders.sample(Examples.dac4, 4)[end]
+    dag = TaskDAGAnalyzers.dag(ctx)
+    unit = DurationNS(123_456_789)
+    set_uniform_unit_work!(dag, unit)
+    expected_work = DurationNS(unit.value * 16)
+    @test TaskDAGAnalyzers.work(dag) == expected_work
 end
 
 end  # module
