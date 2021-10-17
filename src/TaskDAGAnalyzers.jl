e3cc652336310b92f86608b1b582c72d9d925841
Takafumi Arakaki <aka.tkf@gmail.com>
2021-10-17 02:06:23 -0400

    Improve docs

--- src/TaskDAGAnalyzers.jl
+++ src/TaskDAGAnalyzers.jl
@@ -11,10 +11,12 @@ module Internal
 using ..TaskDAGAnalyzers: TaskDAGAnalyzers
 
 using TaskDAGRecorders
-using TaskDAGRecorders.Internal: AnyContext, SyncContext, TaskContext, TaskStat, _get!
+using TaskDAGRecorders.Internal:
+    AnyContext, SyncContext, TaskContext, TaskStat, _get!, get_sync_context
 
 import ShowGraphviz
 
+include("utils.jl")
 include("dag.jl")
 include("graphviz.jl")
 include("summary.jl")
@@ -24,5 +26,7 @@ end  # module Internal
 module Examples
 include("examples.jl")
 end  # module Examples
+
+Internal.define_docstring()
 
 end  # baremodule TaskDAGAnalyzers
