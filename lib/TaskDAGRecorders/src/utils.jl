64bee5278aa00d3690abd3d2e9f1fc29466dc5da
Takafumi Arakaki <aka.tkf@gmail.com>
2021-10-16 21:41:04 -0400

    Define `@var_str`

--- lib/TaskDAGRecorders/src/utils.jl
+++ lib/TaskDAGRecorders/src/utils.jl
@@ -1,6 +1,12 @@
 # TODO: Use Preferences?
 const DEBUG = true
 
+if !isdefined(Base, Symbol("@var_str"))
+    macro var_str(s)
+        Symbol(s)
+    end
+end
+
 if DEBUG
     const var"@_debug" = var"@debug"
 else
